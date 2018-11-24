8c52d6b21b8275b332a1c3fe34983e8c475037d4
Maya Murad <mmurad7@uwo.ca>
2018-11-24 08:52:43 -0500

    New network graph in the works.

--- public/js/index.js
+++ public/js/index.js
@@ -1,164 +1,30 @@
+// create an array with nodes
+ var nodes = new vis.DataSet([
+    {id: 1, label: 'Node 1'},
+    {id: 2, label: 'Node 2'},
+    {id: 3, label: 'Node 3'},
+    {id: 4, label: 'Node 4'},
+    {id: 5, label: 'Node 5'}
+]);
 
+// create an array with edges
+var edges = new vis.DataSet([
+    {from: 1, to: 3},
+    {from: 1, to: 2},
+    {from: 2, to: 4},
+    {from: 2, to: 5}
+]);
 
-var Renderer = function(canvas) {
-    var canvas = $(canvas).get(0)
-    var ctx = canvas.getContext("2d");
-    var particleSystem
+// create a network
+var container = document.getElementById('viewport');
 
-    var that = {
-      init:function(system){
-        //
-        // the particle system will call the init function once, right before the
-        // first frame is to be drawn. it's a good place to set up the canvas and
-        // to pass the canvas size to the particle system
-        //
-        // save a reference to the particle system for use in the .redraw() loop
-        particleSystem = system
+// provide the data in the vis format
+var data = {
+    nodes: nodes,
+    edges: edges
+};
+var options = {};
 
-        // inform the system of the screen dimensions so it can map coords for us.
-        // if the canvas is ever resized, screenSize should be called again with
-        // the new dimensions
-        particleSystem.screenSize(canvas.width, canvas.height) 
-        particleSystem.screenPadding(80) // leave an extra 80px of whitespace per side
-        
-        // set up some event handlers to allow for node-dragging
-        that.initMouseHandling()
-      },
-      
-      redraw:function(){
-        // 
-        // redraw will be called repeatedly during the run whenever the node positions
-        // change. the new positions for the nodes can be accessed by looking at the
-        // .p attribute of a given node. however the p.x & p.y values are in the coordinates
-        // of the particle system rather than the screen. you can either map them to
-        // the screen yourself, or use the convenience iterators .eachNode (and .eachEdge)
-        // which allow you to step through the actual node objects but also pass an
-        // x,y point in the screen's coordinate system
-        // 
+// initialize your network!
+var network = new vis.Network(container, data, options);
 
-        ctx.fillStyle = "white"
-        ctx.fillRect(0,0, canvas.width, canvas.height)
-        
-        particleSystem.eachEdge(function(edge, pt1, pt2){
-          // edge: {source:Node, target:Node, length:#, data:{}}
-          // pt1:  {x:#, y:#}  source position in screen coords
-          // pt2:  {x:#, y:#}  target position in screen coords
-
-          // draw a line from pt1 to pt2
-          ctx.strokeStyle = "purple"
-          ctx.lineWidth = 3
-          ctx.beginPath()
-          ctx.moveTo(pt1.x, pt1.y)
-          ctx.lineTo(pt2.x, pt2.y)
-          ctx.stroke()
-        })
-
-        particleSystem.eachNode(function(node, pt){
-          // node: {mass:#, p:{x,y}, name:"", data:{}}
-          // pt:   {x:#, y:#}  node position in screen coords
-
-          // draw a rectangle centered at pt
-          var w = 35
-          ctx.beginPath();
-          ctx.arc(pt.x, pt.y, w, 0, 2*Math.PI);
-          ctx.lineWidth = 10;
-          ctx.stroke();
-          ctx.fillStyle="pink";
-          ctx.fill()
-          ctx.font="20px Georgia";
-          ctx.lineWidth = 2;
-          let name = node.data.displayString;
-          if (name == null) {
-              name = "hello";
-          }
-          ctx.strokeText(name, pt.x-w/2,pt.y);
-          
-          //ctx.fillStyle = (node.data.alone) ? "pink" : "purple"
-          //ctx.fillRect(pt.x-w/10, pt.y-w/2, w,w)
-        })    			
-      },
-      
-      initMouseHandling:function(){
-        // no-nonsense drag and drop (thanks springy.js)
-        var dragged = null;
-
-        // set up a handler object that will initially listen for mousedowns then
-        // for moves and mouseups while dragging
-        var handler = {
-          clicked:function(e){
-            var pos = $(canvas).offset();
-            _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
-            dragged = particleSystem.nearest(_mouseP);
-
-            if (dragged && dragged.node !== null){
-              // while we're dragging, don't let physics move the node
-              dragged.node.fixed = true
-            }
-
-            $(canvas).bind('mousemove', handler.dragged)
-            $(window).bind('mouseup', handler.dropped)
-
-            return false
-          },
-          dragged:function(e){
-            var pos = $(canvas).offset();
-            var s = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
-
-            if (dragged && dragged.node !== null){
-              var p = particleSystem.fromScreen(s)
-              dragged.node.p = p
-            }
-
-            return false
-          },
-
-          dropped:function(e){
-            if (dragged===null || dragged.node===undefined) return
-            if (dragged.node !== null) dragged.node.fixed = false
-            dragged.node.tempMass = 1000
-            dragged = null
-            $(canvas).unbind('mousemove', handler.dragged)
-            $(window).unbind('mouseup', handler.dropped)
-            _mouseP = null
-            return false
-          }
-        }
-        
-        // start listening
-        $(canvas).mousedown(handler.clicked);
-
-      },
-      
-    }
-    return that
-  }    
-
-  $(document).ready(function(){
-    var sys = arbor.ParticleSystem(1000, 600, 0.5) // create the system with sensible repulsion/stiffness/friction
-    sys.parameters({gravity:true}) // use center-gravity to make the graph settle nicely (ymmv)
-    sys.renderer = Renderer("#viewport") // our newly created renderer will have its .init() method called shortly by sys...
-
-    // add some nodes to the graph and watch it go...
-    // sys.addEdge('a','b')
-    // sys.addEdge('a','c')
-    // sys.addEdge('a','d')
-    // sys.addEdge('a','e')
-
-    // or, equivalently:
-    //
-    sys.graft({
-      nodes:{
-        a: { displayString: "a" },
-        f: { alone:true, mass:.25 }
-      }, 
-      edges:{
-        a: {
-            b:{},
-            c:{},
-            d:{},
-            e:{}
-        }
-      }
-    })
-    
-  })

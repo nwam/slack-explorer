989fd3d39fcfab84332ff00f667689aa9e9248a1
Maya Murad <mmurad7@uwo.ca>
2018-11-24 02:50:21 -0500

    arbor

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -6,7 +6,7 @@ const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express ' });
+    res.render('index', { title: 'Express in my butt', p2: "poo poo" });
 });
 module.exports = router;
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Express ' });
+  res.render('index', { title: 'Express in my butt', p2: "poo poo" });
 });
 
 export = router;

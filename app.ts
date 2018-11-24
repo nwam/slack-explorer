e047df8fea8dddc4324196b583cf8ef251bb7b8c
Tim Etchells <timetchells@ibm.com>
2018-11-24 02:23:31 -0500

    lax tslint

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req: express.Request, res: express.Response, next: express.NextFunction) {
   next(createError(404));
 });
 
--- routes/index.ts
+++ routes/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
   res.render('index', { title: 'Express ' });
 });
 
